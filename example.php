2a107be198e3aa170a6d67b313ee35473d0d61ea
tomfcz <37912558+tomfcz@users.noreply.github.com>
2021-03-02 17:59:33 +0100

    Create example.php

---
+++ example.php
@@ -0,0 +1,14 @@
+<?php
+  $server_ip = '';
+  $server_port = '';
+
+  $cURL = curl_init();
+  curl_setopt($cURL, CURLOPT_URL, "https://api.tomfcz.fr/request_aram3server_steam?ip=$server_ip&port=$server_port");
+  curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
+  $result = curl_exec($cURL);
+  curl_close($cURL);
+
+  $result = json_decode($result, true);
+
+  print_r($result);
+?>
