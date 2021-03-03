2a1a7c28e4f7b8788e475328685d1667c4a80aee
tomfcz <37912558+tomfcz@users.noreply.github.com>
2021-03-03 21:21:42 +0100

    Update example.js

--- example.js
+++ example.js
@@ -1,8 +1,9 @@
 var server_ip = '';
 var server_port = '';
+var token = '';
 
 var xmlHttp = new XMLHttpRequest();
-xmlHttp.open("GET", `https://api.tomfcz.fr/request_aram3server_steam?ip=${server_ip}&port=${server_port}`, false);
+xmlHttp.open("GET", `https://api.tomfcz.fr/request_aram3server_steam?ip=${server_ip}&port=${server_port}&token=${token}`, false);
 xmlHttp.send();
 
 var result = JSON.parse(xmlHttp.responseText);
