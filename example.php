efb519bc24c0eeeeba0280d758cb9df06db5c950
tomfcz <37912558+tomfcz@users.noreply.github.com>
2021-03-03 21:22:07 +0100

    Update example.php

--- example.php
+++ example.php
@@ -1,9 +1,10 @@
 <?php
   $server_ip = '';
   $server_port = '';
+  $token = '';
 
   $cURL = curl_init();
-  curl_setopt($cURL, CURLOPT_URL, "https://api.tomfcz.fr/request_aram3server_steam?ip=$server_ip&port=$server_port");
+  curl_setopt($cURL, CURLOPT_URL, "https://api.tomfcz.fr/request_aram3server_steam?ip=$server_ip&port=$server_port&token=$token");
   curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
   $result = curl_exec($cURL);
   curl_close($cURL);
